7215e574f2b5359023a89aa381be0fb7cc12fe27
Dong Liu <edongliu@gmail.com>
2013-04-24 15:47:13 -0400

    add fontawesome and have hover working

--- public/javascripts/builder.js
+++ public/javascripts/builder.js
@@ -8,12 +8,22 @@ function init () {
   $('#output').sortable({
     placeholder: "ui-state-highlight"
   });
+  $('#output').on('mouseover', '.control-group', function(){
+    $(this).addClass('control-group-highlight');
+    $('.control-group-buttons', this).show();
+  });
+  $('#output').on('mouseout', '.control-group', function(){
+    $(this).removeClass('control-group-highlight');
+    $('.control-group-buttons', this).hide();
+  });
+
   $('#output').disableSelection();
 }
 
 function working () {
   $('#add-checkbox').click(function(e){
     $('#add-item-form').show();
+    cleanko();
     $('#add-item-form legend').text('Checkbox');
     $.data($('#done')[0], 'item', 'checkbox');
     // the working part
@@ -23,57 +33,89 @@ function working () {
       $('#button').show();
     });
     // the output part
-    var element = $('#general-element .control-group').clone().show();
+    var element = $('#checkbox-element .control-group').clone().show();
     $('#output').append(element);
     $('#label').attr("data-bind", "value: label, valueUpdate: 'afterkeydown'");
     $('label span', element).attr("data-bind", "text: label");
     var formModel = function (init) {
       this.label = ko.observable(init);
-    }
+    };
     ko.applyBindings(new formModel("update me"));
     e.preventDefault();
   });
 
   $('#add-text').click(function(e){
     $('#add-item-form').show();
+    cleanko();
     $('#add-item-form legend').text('Text');
     $('.add-item').hide(100, function(){
-      $('#add-item-form legend').show()
+      $('#add-item-form legend').show();
       $('#general').show();
       $('#input').show();
       $('#button').show();
     });
-    var element1 = $('#general-element .control-group').clone().show();
+    var element = $('#text-element .control-group').clone().show();
     $('#output').append(element);
-    var element2 = $('#general-element .control-group').clone().show();
-    $('#output').append(element);
+
     $('#label').attr("data-bind", "value: label, valueUpdate: 'afterkeydown'");
-    $('label span', element).attr("data-bind", "text: label");
+    $('.control-label span', element).attr("data-bind", "text: label");
+
+    $('#placeholder').attr("data-bind", "value: placeholder, valueUpdate: 'afterkeydown'");
+    $('input', element).attr("data-bind", "attr: {placeholder: placeholder}");
+
+    $('#inline').attr("data-bind", "value: inline, valueUpdate: 'afterkeydown'");
+    $('span.help-inline', element).attr("data-bind", "text: inline");
+
     var formModel = function (init) {
       this.label = ko.observable(init);
-    }
-    ko.applyBindings(new formModel("update me"));
+      this.placeholder = ko.observable("");
+      this.inline = ko.observable("");
+    };
+    ko.applyBindings(new formModel("Update me"));
+
     e.preventDefault();
   });
 
   $('#add-par').click(function(e){
     $('#add-item-form').show();
+    cleanko();
     $('#add-item-form legend').text('Paragraph');
     $('.add-item').hide(100, function(){
-      $('#add-item-form legend').show()
+      $('#add-item-form legend').show();
       $('#general').show();
       $('#input').show();
       $('#paragraph').show();
       $('#button').show();
     });
+
+    var element = $('#textarea-element .control-group').clone().show();
+    $('#output').append(element);
+    $('#label').attr("data-bind", "value: label, valueUpdate: 'afterkeydown'");
+    $('#placeholder').attr("data-bind", "value: placeholder, valueUpdate: 'afterkeydown'");
+    $('#rows').attr("data-bind", "value: rows, valueUpdate: 'afterkeydown'");
+    $('#inline').attr("data-bind", "value: inline, valueUpdate: 'afterkeydown'");
+
+    $('span.help-inline', element).attr("data-bind", "text: inline");
+    $('.control-label span', element).attr("data-bind", "text: label");
+    $('textarea', element).attr("data-bind", "attr: {placeholder: placeholder, rows: rows}");
+
+    var formModel = function (init) {
+      this.label = ko.observable(init);
+      this.placeholder = ko.observable("");
+      this.inline = ko.observable("");
+      this.rows = ko.observable(3);
+    };
+    ko.applyBindings(new formModel("Update me"));
+
     e.preventDefault();
   });
 
   $('#add-int').click(function(e){
     $('#add-item-form').show();
+    cleanko();
     $('#add-item-form legend').text('Integer');
     $('.add-item').hide(100, function(){
-      $('#add-item-form legend').show()
+      $('#add-item-form legend').show();
       $('#general').show();
       $('#input').show();
       $('#number').show();
@@ -83,9 +125,10 @@ function working () {
 
   $('#add-float').click(function(e){
     $('#add-item-form').show();
+    cleanko();
     $('#add-item-form legend').text('Float');
     $('.add-item').hide(100, function(){
-      $('#add-item-form legend').show()
+      $('#add-item-form legend').show();
       $('#general').show();
       $('#input').show();
       $('#number').show();
@@ -95,9 +138,10 @@ function working () {
 
   $('#add-file').click(function(e){
     $('#add-item-form').show();
+    cleanko();
     $('#add-item-form legend').text('File');
     $('.add-item').hide(100, function(){
-      $('#add-item-form legend').show()
+      $('#add-item-form legend').show();
       $('#upload').show();
       $('#button').show();
     });
@@ -106,9 +150,10 @@ function working () {
 
   $('#add-link').click(function(e){
     $('#add-item-form').show();
+    cleanko();
     $('#add-item-form legend').text('Link');
     $('.add-item').hide(100, function(){
-      $('#add-item-form legend').show()
+      $('#add-item-form legend').show();
       $('#link').show();
       $('#button').show();
     });
@@ -117,9 +162,10 @@ function working () {
 
   $('#add-rich').click(function(e){
     $('#add-item-form').show();
+    cleanko();
     $('#add-item-form legend').text('Rich instruction');
     $('.add-item').hide(100, function(){
-      $('#add-item-form legend').show()
+      $('#add-item-form legend').show();
       $('#rich').show();
       $('#button').show();
     });
@@ -128,9 +174,10 @@ function working () {
 
   $('#add-hold').click(function(e){
     $('#add-item-form').show();
+    cleanko();
     $('#add-item-form legend').text('Hold point');
     $('.add-item').hide(100, function(){
-      $('#add-item-form legend').show()
+      $('#add-item-form legend').show();
       $('#hold').show();
       $('#button').show();
     });
@@ -139,41 +186,26 @@ function working () {
 
   $('#done').click(function(e){
     // clean the ko binding
-    // ko.cleanNode($('#output')[0]);
-    ko.cleanNode($('#label')[0]);
-    $('#output [data-bind]').removeAttr('data-bind');
-    $('#add-item-form [data-bind]').removeAttr('data-bind');
+    cleanko();
     // hide the add-item-form
     $('.add-item').hide();
     $('#add-item-form').hide();
 
-    // console.log($.data($('#add')[0], 'item'));
-    // var item = $.data($('#done')[0], 'item');
-    // var element;
-    // switch(item) {
-    //   case 'checkbox':
-        // console.log($.data($('#add')[0], 'item'));
-        // element = $('#checkbox-element .control-group').clone().show();
-        // $('.control-label', element).html($('#label').val());
-        // $('#output').append(element);
-        // $('.add-item').hide();
-        // $('#add-item-form').hide();
-        // // break;
-    //   default:
-    //     $('.add-item').hide();
-    //     $('#add-item-form').hide();
-    //     break;
+    // $('#output .control-group').hover(
+    // function(){
+    //   $(this).addClass('control-group-highlight');
+    // },
+    // function(){
+    //   $(this).removeClass('control-group-highlight');
     // }
+    // );
+
     e.preventDefault();
-    // return false;
   });
 
   $('#cancel').click(function(e){
     // clean the ko binding
-    // ko.cleanNode($('#output')[0]);
-    ko.cleanNode($('#label')[0]);
-    $('#output [data-bind]').removeAttr('data-bind');
-    $('#add-item-form [data-bind]').removeAttr('data-bind');
+    cleanko();
 
     // remove the last element just appended
     $('#output .control-group').last().remove();
@@ -182,25 +214,16 @@ function working () {
     $('.add-item').hide();
     $('#add-item-form').hide();
 
-    // console.log($.data($('#add')[0], 'item'));
-    // var item = $.data($('#done')[0], 'item');
-    // var element;
-    // switch(item) {
-    //   case 'checkbox':
-        // console.log($.data($('#add')[0], 'item'));
-        // element = $('#checkbox-element .control-group').clone().show();
-        // $('.control-label', element).html($('#label').val());
-        // $('#output').append(element);
-        // $('.add-item').hide();
-        // $('#add-item-form').hide();
-        // // break;
-    //   default:
-    //     $('.add-item').hide();
-    //     $('#add-item-form').hide();
-    //     break;
-    // }
     e.preventDefault();
-    // return false;
   });
 
+}
+
+function cleanko() {
+  $('#add-item-form input').each(function(index) {
+    ko.cleanNode(this);
+  });
+
+  $('#output [data-bind]').removeAttr('data-bind');
+  $('#add-item-form [data-bind]').removeAttr('data-bind');
 }
