b39e9b41c6530553c1b492e27e6feab0acde68a3
Dong Liu <edongliu@gmail.com>
2013-11-04 09:51:31 -0500

    checkbox edit tested

--- public/javascripts/builder.js
+++ public/javascripts/builder.js
@@ -1,6 +1,6 @@
 var mce_head = {
   inline: true,
-  browser_spellcheck : true,
+  browser_spellcheck: true,
   plugins: "charmap",
   toolbar: "undo redo | subscript superscript charmap",
   menubar: false,
@@ -9,10 +9,12 @@ var mce_head = {
 
 var mce_content = {
   inline: true,
-  browser_spellcheck : true,
-  plugins: [["advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker"],
-      ["searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking"],
-      ["save table contextmenu directionality emoticons template paste"]],
+  browser_spellcheck: true,
+  plugins: [
+    ["advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker"],
+    ["searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking"],
+    ["save table contextmenu directionality emoticons template paste"]
+  ],
   toolbar1: "subscript superscript charmap | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
   toolbar2: "undo redo | removeformat | fontselect fontsizeselect | bold italic underline strikethrough",
   contextmenu: "charmap link image",
@@ -26,44 +28,24 @@ $(function() {
   binding_events();
 });
 
-function init () {
+function init() {
   $('#add-item-form').hide();
   $('#form-title h3.tinymce').tinymce(mce_head);
   $('#form-title div.tinymce').tinymce(mce_content);
 }
 
-function working () {
-  $('#add-checkbox').click(function(e){
+function working() {
+  $('#add-checkbox').click(function(e) {
     e.preventDefault();
-    // remove the opened .well.spec
-    $('#output .well.spec').remove();
-    var $checkbox = $(input.checkbox());
-    $('#output').append($('<div class="control-group-wrap"></div>').append($checkbox));
-    var $label = $(spec.label());
-    var $checkbox_text = $(spec.checkbox_text());
-    // var type = $(spec.type());
-    // type.val('checkbox');
-    var $done = $(spec.done());
-    var $edit = $('<div class="well spec"></div>').append($label, $checkbox_text, $done);
-    $('#output').append($edit);
-    var model = {
-      label: 'label',
-      checkbox_text: 'checkbox text'
-    };
-    $('input', $edit).keyup(function(e){
-      model[$(this).attr('name')] = $(this).val();
-    });
-    var view = rivets.bind($checkbox, {
-      model: model
-    });
-    $done.click(done_button(view));
+    checkbox_edit();
   });
 
-  $('#add-text').click(function(e){
+
+  $('#add-text').click(function(e) {
     e.preventDefault();
     $('#output .well.spec').remove();
     var $text = $(input.text());
-    $('#output').append($('<div class="control-group-wrap"></div>').append($text));
+    $('#output').append($('<div class="control-group-wrap" data-type="text"></div>').append($text));
     var $label = $(spec.label());
     var $placeholder = $(spec.placeholder());
     var $help = $(spec.help());
@@ -75,7 +57,7 @@ function working () {
       placeholder: '',
       help: ''
     };
-    $('input', $edit).keyup(function(e){
+    $('input', $edit).keyup(function(e) {
       model[$(this).attr('name')] = $(this).val();
     });
     var view = rivets.bind($text, {
@@ -84,11 +66,11 @@ function working () {
     $done.click(done_button(view));
   });
 
-  $('#add-par').click(function(e){
+  $('#add-par').click(function(e) {
     e.preventDefault();
     $('#output .well.spec').remove();
     var $textarea = $(input.textarea());
-    $('#output').append($('<div class="control-group-wrap"></div>').append($textarea));
+    $('#output').append($('<div class="control-group-wrap" data-type="textarea"></div>').append($textarea));
     var $label = $(spec.label());
     var $placeholder = $(spec.placeholder());
     var $rows = $(spec.rows());
@@ -102,7 +84,7 @@ function working () {
       rows: 3,
       help: ''
     };
-    $('input', $edit).keyup(function(e){
+    $('input', $edit).keyup(function(e) {
       model[$(this).attr('name')] = $(this).val();
     });
     var view = rivets.bind($textarea, {
@@ -111,11 +93,11 @@ function working () {
     $done.click(done_button(view));
   });
 
-  $('#add-number').click(function(e){
+  $('#add-number').click(function(e) {
     e.preventDefault();
     $('#output .well.spec').remove();
     var $number = $(input.number());
-    $('#output').append($('<div class="control-group-wrap"></div>').append($number));
+    $('#output').append($('<div class="control-group-wrap" data-type="number"></div>').append($number));
     var $label = $(spec.label());
     var $placeholder = $(spec.placeholder());
     var $help = $(spec.help());
@@ -127,7 +109,7 @@ function working () {
       placeholder: '',
       help: ''
     };
-    $('input', $edit).keyup(function(e){
+    $('input', $edit).keyup(function(e) {
       model[$(this).attr('name')] = $(this).val();
     });
     var view = rivets.bind($textarea, {
@@ -136,11 +118,11 @@ function working () {
     $done.click(done_button(view));
   });
 
-  $('#add-file').click(function(e){
+  $('#add-file').click(function(e) {
     e.preventDefault();
     $('#output .well.spec').remove();
     var $upload = $(input.upload());
-    $('#output').append($('<div class="control-group-wrap"></div>').append($upload));
+    $('#output').append($('<div class="control-group-wrap" data-type="file"></div>').append($upload));
     var $label = $(spec.label());
     var $help = $(spec.help());
     var $done = $(spec.done());
@@ -150,7 +132,7 @@ function working () {
       label: 'label',
       help: ''
     };
-    $('input', $edit).keyup(function(e){
+    $('input', $edit).keyup(function(e) {
       model[$(this).attr('name')] = $(this).val();
     });
     var view = rivets.bind($upload, {
@@ -159,40 +141,40 @@ function working () {
     $done.click(done_button(view));
   });
 
-  $('#add-rich').click(function(e){
+  $('#add-rich').click(function(e) {
     e.preventDefault();
     $('#output .well.spec').remove();
     var $rich = $(input.rich());
-    $('#output').append($('<div class="control-group-wrap"></div>').append($rich));
+    $('#output').append($('<div class="control-group-wrap" data-type="rich"></div>').append($rich));
     $rich.tinymce(mce_content);
   });
 
-//   $('#add-hold').click(function(e){
-//     clean_ko();
-//     $('#add-item-form').empty();
-//     $('#add-item-form').show();
-//     var legend = $('#legend legend').clone().text('Add hold point').show();
-//     var hold = $('#hold .control-group').clone().show();
-//     var button = $('#button .form-actions').clone().show();
-//     $('#add-item-form').append(legend);
-//     $('#add-item-form').append(hold);
-//     $('#add-item-form').append(button);
+  //   $('#add-hold').click(function(e){
+  //     clean_ko();
+  //     $('#add-item-form').empty();
+  //     $('#add-item-form').show();
+  //     var legend = $('#legend legend').clone().text('Add hold point').show();
+  //     var hold = $('#hold .control-group').clone().show();
+  //     var button = $('#button .form-actions').clone().show();
+  //     $('#add-item-form').append(legend);
+  //     $('#add-item-form').append(hold);
+  //     $('#add-item-form').append(button);
 
-//     // the output part
-//     var element = $('#hold-element .control-group').clone().show();
-//     element.prepend($('#control-group-buttons .btn-group').clone());
-//     $('#output').append(element);
-//     $('input', hold).attr("data-bind", "value: label, valueUpdate: 'afterkeydown'");
-//     $('.holder span', element).attr("data-bind", "text: label");
-//     var formModel = function (init) {
-//       this.label = ko.observable(init);
-//     };
-//     ko.applyBindings(new formModel("Name"));
-//     binding_button();
-//     e.preventDefault();
-//   });
+  //     // the output part
+  //     var element = $('#hold-element .control-group').clone().show();
+  //     element.prepend($('#control-group-buttons .btn-group').clone());
+  //     $('#output').append(element);
+  //     $('input', hold).attr("data-bind", "value: label, valueUpdate: 'afterkeydown'");
+  //     $('.holder span', element).attr("data-bind", "text: label");
+  //     var formModel = function (init) {
+  //       this.label = ko.observable(init);
+  //     };
+  //     ko.applyBindings(new formModel("Name"));
+  //     binding_button();
+  //     e.preventDefault();
+  //   });
 
-  $('#add-section').click(function(e){
+  $('#add-section').click(function(e) {
     e.preventDefault();
     $('#output .well.spec').remove();
     var $section = $(input.section());
@@ -204,7 +186,7 @@ function working () {
     var model = {
       legend: 'Section name',
     };
-    $('input', $edit).keyup(function(e){
+    $('input', $edit).keyup(function(e) {
       model[$(this).attr('name')] = $(this).val();
     });
     var view = rivets.bind($section, {
@@ -214,14 +196,14 @@ function working () {
   });
 
 
-//   $('#add-page').click(function(e){
-//     e.preventDefault();
-//   });
+  //   $('#add-page').click(function(e){
+  //     e.preventDefault();
+  //   });
 
 }
 
 function binding_events() {
-  $('#adjust').click(function(e){
+  $('#adjust').click(function(e) {
     if ($(this).text() == 'Adjust') {
       $(this).text('Done');
       $('#input-items').attr('disabled', true);
@@ -239,25 +221,25 @@ function binding_events() {
     }
     e.preventDefault();
   });
-  $('#output').on('mouseenter', '.control-group-wrap', function(e){
+  $('#output').on('mouseenter', '.control-group-wrap', function(e) {
     e.preventDefault();
     if (!$(this).hasClass('control-focus')) {
       $(this).addClass('control-focus');
       $(this).prepend(input.button());
     }
   });
-  $('#output').on('mouseleave', '.control-group-wrap', function(e){
+  $('#output').on('mouseleave', '.control-group-wrap', function(e) {
     e.preventDefault();
     if ($(this).hasClass('control-focus')) {
       $(this).removeClass('control-focus');
       $('.control-group-buttons', $(this)).remove();
     }
   });
-  $('#output').on('click', '.control-focus a.btn.btn-warning[title="remove"]', function(e){
+  $('#output').on('click', '.control-focus a.btn.btn-warning[title="remove"]', function(e) {
     e.preventDefault();
     $(this).closest('.control-group-wrap').remove();
   });
-  $('#output').on('click', '.control-focus a.btn[title="duplicate"]', function(e){
+  $('#output').on('click', '.control-focus a.btn[title="duplicate"]', function(e) {
     e.preventDefault();
     var that = this;
     var cloned = $(that).closest('.control-group-wrap').clone();
@@ -266,30 +248,23 @@ function binding_events() {
     $(that).closest('.control-group-wrap').after(cloned);
   });
 
-  $('#output').on('click', '.control-focus a.btn[title="edit"]', function(e){
+  $('#output').on('click', '.control-focus a.btn[title="edit"]', function(e) {
     e.preventDefault();
-    var cg = $(this).closest('.control-group');
-    var type = $('.control-group').attr('data-type');
-    // var spec_form = $(edit_form($('.control-group').attr('data-type')));
-    // some input types do not need the spec form
-    if (['rich'].indexOf(type) !== -1 ) {
-      alert('Please edit it directly.');
-    } else {
-      $(this).closest('.control-group-wrap').after();
-      binding($(this).closest('.control-group'), spec_form);
+    var $cgr = $(this).closest('.control-group-wrap');
+    var type = $cgr.attr('data-type');
+    switch (type) {
+      case 'rich':
+        alert('Please edit it inline.');
+        break;
+      case 'checkbox':
+        checkbox_edit($cgr);
+        break;
+      default:
+        alert('not implemented.');
     }
   });
 
 }
-
-// function clean_ko() {
-//   $('#output .well.spec input').each(function(index) {
-//     ko.cleanNode(this);
-//   });
-
-//   $('#output [data-bind]').removeAttr('data-bind');
-//   $('#add-item-form [data-bind]').removeAttr('data-bind');
-// }
 
 function done_button(view) {
   return function(e) {
@@ -299,3 +274,39 @@ function done_button(view) {
   };
 }
 
+function checkbox_edit($cgr) {
+  $('#output .well.spec').remove();
+  var label = 'label';
+  var checkbox_text = 'checkbox text';
+  if ($cgr) {
+    label = $('.control-label span', $cgr).text();
+    checkbox_text = $('.controls label span', $cgr).text();
+  }
+  var $checkbox = $(input.checkbox());
+  var $label = $(spec.label());
+  var $checkbox_text = $(spec.checkbox_text());
+  var $done = $(spec.done());
+  var $edit = $('<div class="well spec"></div>').append($label, $checkbox_text, $done);
+  if ($cgr) {
+    var $new_cgr = $('<div class="control-group-wrap" data-type="checkbox"></div>').append($checkbox);
+    $cgr.replaceWith($new_cgr);
+    $new_cgr.after($edit);
+  } else {
+    $('#output').append($('<div class="control-group-wrap" data-type="checkbox"></div>').append($checkbox));
+    $('#output').append($edit);
+  }
+  var model = {
+    label: label,
+    checkbox_text: checkbox_text
+  };
+  $('input', $label).val(label);
+  $('input', $checkbox_text).val(checkbox_text);
+
+  $('input', $edit).keyup(function(e) {
+    model[$(this).attr('name')] = $(this).val();
+  });
+  var view = rivets.bind($checkbox, {
+    model: model
+  });
+  $done.click(done_button(view));
+}
