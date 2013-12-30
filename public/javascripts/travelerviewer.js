3e98a3ad9cd003d3ddf5dd8922322e1f576507bd
Dong Liu <edongliu@gmail.com>
2013-12-30 16:20:23 -0500

    update viewer for file inputs

--- public/javascripts/travelerviewer.js
+++ public/javascripts/travelerviewer.js
@@ -1,40 +1,55 @@
-$(function() {
+function history(found) {
+  var i, output = '';
+  if (found.length > 0) {
+    for (i = 0; i < found.length; i += 1) {
+      output = output + 'changed to <strong>' + found[i].value + '</strong> by ' + found[i].inputBy + ' ' + moment(found[i].inputOn).fromNow() + '; ';
+    }
+  }
+  return output;
+}
+
+function fileHistory(found) {
+  var i, output = '',
+    link;
+  if (found.length > 0) {
+    for (i = 0; i < found.length; i += 1) {
+      link = '/data/' + found[i]._id;
+      output = output + '<strong><a href=' + link + ' target="_blank">' + found[i].value + '</a></strong> uploaded by ' + found[i].inputBy + ' ' + moment(found[i].inputOn).fromNow() + '; ';
+    }
+  }
+  return output;
+}
+
+$(function () {
   var binder = new Binder.FormBinder(document.forms[0]);
   $.ajax({
     url: './data/',
     type: 'GET',
     dataType: 'json'
-  }).done(function(data, status, jqXHR) {
-    $('#form input,textarea').each(function(index, element) {
-      var found = data.filter(function(e) {
-        return e.name == element.name;
+  }).done(function (data, status, jqXHR) {
+    $('#form input,textarea').each(function (index, element) {
+      var found = data.filter(function (e) {
+        return e.name === element.name;
       });
       if (found.length) {
-        found.sort(function(a, b) {
+        found.sort(function (a, b) {
           if (a.inputOn > b.inputOn) {
             return -1;
-          } else {
-            return 1;
           }
+          return 1;
         });
-        binder.deserializeFieldFromValue(element, found[0].value);
-        binder.accessor.set(element.name, found[0].value);
-        $(element).closest('.controls').append('<div class="history">' + history(found) + '</div>');
+        if (this.type == 'file') {
+          $(element).closest('.controls').append('<div class="history">' + fileHistory(found) + '</div>');
+        } else {
+          binder.deserializeFieldFromValue(element, found[0].value);
+          binder.accessor.set(element.name, found[0].value);
+          $(element).closest('.controls').append('<div class="history">' + history(found) + '</div>');
+        }
       }
     });
-  }).fail(function(jqXHR, status, error) {
+  }).fail(function (jqXHR, status, error) {
     $('#message').append('<div class="alert alert-error"><button class="close" data-dismiss="alert">x</button>Cannot get saved traveler data</div>');
     $(window).scrollTop($('#message div:last-child').offset().top - 40);
   }).always();
 
 });
-
-function history(found) {
-  var output = 'changed by ' + found[0].inputBy + ' ' + moment(found[0].inputOn).fromNow();
-  if (found.length > 1) {
-    for (var i = 1; i < found.length; i += 1) {
-      output = output + '; changed to <strong>' + found[i].value + '</strong> by ' + found[i].inputBy + ' ' + moment(found[i].inputOn).fromNow();
-    }
-  }
-  return output;
-}
