7629530293017d10309ef4e062712546876a4312
Dong Liu <edongliu@gmail.com>
2014-02-06 12:11:32 -0500

    add styles for section affix, enable affix in viewer.js

--- public/javascripts/viewer.js
+++ public/javascripts/viewer.js
@@ -1,3 +1,15 @@
 $(function(){
   $('input, textarea').removeAttr('disabled');
+  var $legend = $('legend');
+  var $affix = $('<ul class="nav nav-list nav-stacked affix bs-docs-sidenav" data-offset-top="0"></ul>');
+  var i;
+  if ($legend.length > 1) {
+    // $legend.attr('data-spy', 'affix');
+    for (i = 0; i < $legend.length; i += 1) {
+      $affix.append('<li><a href="#' + $legend[i].id + '">' + $legend[i].textContent + '</a></li>');
+    }
+    $('body').append($('<div id="affixlist" class="bs-docs-sidebar"></div>').append($affix));
+    $('body').attr('data-spy', 'scroll');
+    $('body').attr('data-target', '#affixlist');
+  }
 });
