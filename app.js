83e82ea21d0c1b7b3b867b2f826d36050788e0d7
Dong Liu <edongliu@gmail.com>
2014-03-26 16:38:41 -0400

    add compress middleware to app chain

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   // app.use(express.logger({stream: access_logfile}));
+  app.use(express.compress());
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.favicon(__dirname + '/public/favicon.ico'));
   app.use(express.logger('dev'));
