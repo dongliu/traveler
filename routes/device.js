21d9e873fab1ee94f08e50e336d2a197e8cf4c36
Dong Liu <liud@frib.msu.edu>
2014-10-15 09:01:47 -0400

    handle error in device proxy to prevent xhr session issue

--- routes/device.js
+++ routes/device.js
@@ -2,17 +2,26 @@ var auth = require('../lib/auth');
 
 var request = require('request');
 
-module.exports = function(app) {
-  app.get('/devices/', function(req, res) {
+module.exports = function (app) {
+  app.get('/devices/', function (req, res) {
     res.render('devices');
   });
 
-  app.get('/devices/json', function(req, res) {
+  app.get('/devices/json', function (req, res) {
     request({
       url: 'http://ctlapp-wheezy-temp:8080/conf/rs/v0/component/physical',
+      timeout: 30 * 1000,
       headers: {
         Accept: 'application/json'
       }
-    }).pipe(res);
+    }, function (err, response, resBody) {
+      if (err) {
+        console.log(err);
+        return res.json(503, {
+          error: 'cannot retrieve device list from ctlapp-wheezy-temp'
+        });
+      }
+      return res.json(response.statusCode, resBody);
+    });
   });
 };
