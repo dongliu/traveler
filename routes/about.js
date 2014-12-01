7eaa713eaa59c576e958cfeb9ead5e3119659055
Dong Liu <liud@frib.msu.edu>
2014-12-01 12:52:36 -0500

    proxy prefix about and layout

--- routes/about.js
+++ routes/about.js
@@ -1,8 +1,9 @@
-
 /*
  * GET about page.
  */
 
-exports.index = function(req, res){
-  res.render('about');
+exports.index = function (req, res) {
+  res.render('about', {
+    prefix: req.proxied ? req.proxied_prefix : ''
+  });
 };
