aa42b72b0726c4b4f07b6f916895d41be0d22127
Dong Liu <edongliu@gmail.com>
2014-12-01 13:22:08 -0500

    proxy prefix user nad jslint

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -1,11 +1,23 @@
-$(function() {
+/*global updateAjaxURL: false, prefix: false, roles: true, moment: false*/
+
+
+function update(roles) {
+  var i;
+  for (i = 0; i < roles.length; i += 1) {
+    $('input[value= "' + roles[i] + '"]').prop('checked', true);
+  }
+}
+
+$(function () {
+  updateAjaxURL(prefix);
+
   update(roles);
-  $('#modify').click(function(e) {
+  $('#modify').click(function (e) {
     var currentRoles = [];
-    $('#roles input:checked').each(function() {
+    $('#roles input:checked').each(function () {
       currentRoles.push($(this).val());
     });
-    if (roles.sort().join() == currentRoles.sort().join()) {
+    if (roles.sort().join() === currentRoles.sort().join()) {
       $('#message').append('<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>Nothing was changed.</div>');
     } else {
       var request = $.ajax({
@@ -18,22 +30,15 @@ $(function() {
         contentType: 'application/json',
         processData: false,
         dataType: 'json'
-      }).done(function(json) {
+      }).done(function (json) {
         roles = currentRoles;
         var timestamp = request.getResponseHeader('Date');
         var dateObj = moment(timestamp);
         $('#message').append('<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>The modification was saved at ' + dateObj.format('HH:mm:ss') + '.</div>');
-      }).fail(function(jqXHR, status, error) {
-        // TODO change to modal
-        alert('The save request failed. You might need to try again or contact the admin.');
-      }).always(function() {});
+      }).fail(function (jqXHR, status, error) {
+        $('#message').append('<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>The save request failed. You might need to try again or contact the admin.</div>');
+      }).always(function () {});
     }
     e.preventDefault();
   });
 });
-
-function update(roles) {
-  for (var i = 0; i < roles.length; i += 1) {
-    $('input[value= "' + roles[i] + '"]').prop('checked', true);
-  }
-}
