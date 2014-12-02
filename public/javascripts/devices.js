0766294000750e1b880468e49e68a15b35201ac2
Dong Liu <liud@frib.msu.edu>
2014-12-02 14:17:59 -0500

    proxy prefix devicepage

--- public/javascripts/devices.js
+++ public/javascripts/devices.js
@@ -1,9 +1,9 @@
-$(function(){
+/*global deviceTravelerLinkColumn: false, serialColumn: false, typeColumn: false, descriptionColumn: false, modifiedByColumn: false, modifiedOnColumn: false, fnAddFilterFoot: false, sDom: false, oTableTools: false, filterEvent: false, prefix: false*/
+$(function () {
   var aoColumns = [deviceTravelerLinkColumn, serialColumn, typeColumn, descriptionColumn, modifiedByColumn, modifiedOnColumn];
   fnAddFilterFoot('#device-table', aoColumns);
   var deviceTable = $('#device-table').dataTable({
     aaData: [],
-    // 'bAutoWidth': false,
     aoColumns: aoColumns,
     aaSorting: [
       [5, 'desc']
@@ -15,14 +15,14 @@ $(function(){
   filterEvent();
 
   $.ajax({
-    url: '/devices/json',
+    url: prefix + '/devices/json',
     type: 'GET',
     dataType: 'json'
-  }).done(function(json) {
+  }).done(function (json) {
     deviceTable.fnClearTable();
     deviceTable.fnAddData(json);
     deviceTable.fnDraw();
-  }).fail(function(jqXHR, status, error) {
+  }).fail(function (jqXHR, status, error) {
     $('#message').append('<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>Cannot reach the server for device information.</div>');
   }).always();
 });
