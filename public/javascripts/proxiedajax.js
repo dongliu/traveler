3c086cedb6fd45aa2b325c7f741c9f0103da26f2
Dong Liu <liud@frib.msu.edu>
2014-12-02 16:08:52 -0500

    add handle ajax 401 here

--- public/javascripts/proxiedajax.js
+++ public/javascripts/proxiedajax.js
@@ -1,7 +1,18 @@
+/*global window: false*/
+
 function updateAjaxURL(prefix) {
   if (prefix) {
     $.ajaxPrefilter(function (options) {
       options.url = prefix + options.url;
     });
   }
+}
+
+function ajax401(prefix) {
+  $(document).ajaxError(function (event, jqXHR, settings, exception) {
+    if (jqXHR.status === 401) {
+      $('#message').append('<div class="alert alert-error"><button class="close" data-dismiss="alert">x</button>Please click <a href="' + prefix + '/" target="_blank">travler home</a>, log in, and then save the changes on this page.</div>');
+      $(window).scrollTop($('#message div:last-child').offset().top - 40);
+    }
+  });
 }
