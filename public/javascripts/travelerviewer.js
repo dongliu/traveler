dbeedaedcb8ae3aa6ee8f8b68acef0f7b61d81fc
Dong Liu <liud@frib.msu.edu>
2014-12-03 11:17:44 -0500

    proxy prefix traveler viewer

--- public/javascripts/travelerviewer.js
+++ public/javascripts/travelerviewer.js
@@ -1,5 +1,5 @@
 /*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false, FormData: false */
-/*global moment: false, Binder: false, Modernizr: false*/
+/*global moment: false, Binder: false, Modernizr: false, prefix: false*/
 
 // temparary solution for the dirty forms
 function cleanForm() {
@@ -21,7 +21,7 @@ function fileHistory(found) {
     link;
   if (found.length > 0) {
     for (i = 0; i < found.length; i += 1) {
-      link = '/data/' + found[i]._id;
+      link = prefix + '/data/' + found[i]._id;
       output = output + '<strong><a href=' + link + ' target="_blank">' + found[i].value + '</a></strong> uploaded by ' + found[i].inputBy + ' ' + moment(found[i].inputOn).fromNow() + '; ';
     }
   }
