72d903bff15435f1833604ad068afebd166009cc
Dong Liu <liud@frib.msu.edu>
2015-04-30 14:02:47 -0400

    problem fixed for 0.7.1

--- routes/user.js
+++ routes/user.js
@@ -357,8 +357,7 @@ module.exports = function (app) {
     };
     ldapClient.search(ad.groupSearchBase, opts, false, function (err, result) {
       if (err) {
-        console.error(err);
-        return res.json(500, err);
+        return res.send(500, err.message);
       }
       if (result.length === 0) {
         return res.json([]);
