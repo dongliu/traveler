38e65b02cee5983d937e6417e611554023a0d531
Dong Liu <liud@frib.msu.edu>
2015-06-04 11:53:32 -0400

    use login for ajax 404

--- public/javascripts/proxiedajax.js
+++ public/javascripts/proxiedajax.js
@@ -14,7 +14,7 @@ function updateAjaxURL(prefix) {
 function ajax401(prefix) {
   $(document).ajaxError(function (event, jqXHR, settings, exception) {
     if (jqXHR.status === 401) {
-      $('#message').append('<div class="alert alert-error"><button class="close" data-dismiss="alert">x</button>Please click <a href="' + prefix + '/" target="_blank">travler home</a>, log in, and then save the changes on this page.</div>');
+      $('#message').append('<div class="alert alert-error"><button class="close" data-dismiss="alert">x</button>Please click <a href="' + prefix + '/login" target="_blank">login</a>, log in, and then save the changes on this page.</div>');
       $(window).scrollTop($('#message div:last-child').offset().top - 40);
     }
   });
