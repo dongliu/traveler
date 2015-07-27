534af923a19929f0992a3a8ea60ab0665a79aa17
Dong Liu <liud@frib.msu.edu>
2015-07-27 12:08:28 -0400

    correct name for working package

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ require('./routes/form')(app);
 
 require('./routes/traveler')(app);
 
-require('./routes/work')(app);
+require('./routes/working_package')(app);
 
 require('./routes/user')(app);
 
