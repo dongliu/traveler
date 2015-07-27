76d992091f8f40b6a53c0c3bcb3a1b53dc5919fb
Dong Liu <liud@frib.msu.edu>
2015-07-27 15:24:17 -0400

    new package ready

--- routes/working_package.js
+++ routes/working_package.js
@@ -8,6 +8,7 @@ var auth = require('../lib/auth');
 var authConfig = require('../config/auth.json');
 var mongoose = require('mongoose');
 var util = require('util');
+var underscore = require('underscore');
 var path = require('path');
 require('../model/working_package.js');
 
@@ -509,6 +510,36 @@ module.exports = function (app) {
         return res.send(500, err.message);
       }
       return res.json(200, docs);
+    });
+  });
+
+  app.get('/workingpackages/new', auth.ensureAuthenticated, function (req, res) {
+    res.render('new_package');
+  });
+
+  app.post('/workingpackages/', auth.ensureAuthenticated, function (req, res) {
+    var workingPackage = {};
+    if (!!req.body.works && underscore.isArray(req.body.works)) {
+      workingPackage.works =  req.body.works;
+    } else {
+      workingPackage.works = [];
+    }
+
+    workingPackage.title = req.body.title;
+    if (!!req.body.description) {
+      workingPackage.description = req.body.description;
+    }
+    workingPackage.createdBy = req.session.userid;
+    workingPackage.createdOn = Date.now();
+    (new WorkingPackage(workingPackage)).save(function (err, newPackage) {
+      if (err) {
+        console.error(err);
+        return res.send(500, err.message);
+      }
+      var url = (req.proxied ? authConfig.proxied_service : authConfig.service) + '/workingpackages/' + newPackage.id + '/';
+
+      res.set('Location', url);
+      return res.send(201, 'You can access the new packace at <a href="' + url + '">' + url + '</a>');
     });
   });
 
