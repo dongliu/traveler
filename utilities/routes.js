6db60747bbafb315c40de096951849e0971b9ae2
Dariusz <dariuszjarosz@me.com>
2015-11-06 13:37:36 -0600

    Add function that will be reused to utilties and implement the new version in traveler route.

--- utilities/routes.js
+++ utilities/routes.js
@@ -1,3 +1,30 @@
 /**
  * Created by djarosz on 11/6/15.
  */
+/**
+ * The purpose of this file is to store all functions and utilities that are used by multiple routes.
+ */
+
+function filterBody(strings) {
+    return function (req, res, next) {
+        var k, found = false;
+        for (k in req.body) {
+            if (req.body.hasOwnProperty(k)) {
+                if (strings.indexOf(k) !== -1) {
+                    found = true;
+                } else {
+                    req.body[k] = null;
+                }
+            }
+        }
+        if (found) {
+            next();
+        } else {
+            return res.send(400, 'cannot find required information in body');
+        }
+    };
+}
+
+module.exports = {
+    filterBody: filterBody
+};
