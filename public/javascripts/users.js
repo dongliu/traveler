bb4db3aff11e652543a591a7f1b76520dfa07cbd
Dariusz <dariuszjarosz@me.com>
2015-11-09 08:44:34 -0600

    Check the role to write_active_travelers

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -130,7 +130,7 @@ $(function () {
           manager: $('#manager').prop('checked'),
           admin: $('#admin').prop('checked'),
           read_all_forms: $('#read_all_forms').prop('checked'),
-          write_all_travelers: $('#write_all_travelers').prop('checked')
+          write_active_travelers: $('#write_active_travelers').prop('checked')
         }),
         success: function (data, status, jqXHR) {
           $('#message').append('<div class="alert alert-success"><button class="close" data-dismiss="alert">x</button>' + jqXHR.responseText + '</div>');
@@ -180,7 +180,7 @@ $(function () {
           '<label class="checkbox"><input id="modal-manager" type="checkbox" value="manager">manager</label> ' +
           '<label class="checkbox"><input id="modal-admin" type="checkbox" value="admin">admin</label> ' +
           '<label class="checkbox"><input id="read_all_forms" type="checkbox" value="read_all_forms">read_all_forms</label> ' +
-          '<label class="checkbox"><input id="write_all_travelers" type="checkbox" value="write_all_travelers">write_all_travelers</label> ' +
+          '<label class="checkbox"><input id="write_active_travelers" type="checkbox" value="write_active_travelers">write_active_travelers</label> ' +
           '</form>');
       selected.forEach(function (row) {
         var data = userTable.fnGetData(row);
