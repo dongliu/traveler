95e78a142e3e607b04c29325b731a7b4f1a32873
Dariusz <dariuszjarosz@me.com>
2015-11-30 15:18:58 -0600

    Use the new configuration to show proper devices page

--- routes/device.js
+++ routes/device.js
@@ -6,7 +6,16 @@ var routesUtilities = require('../utilities/routes.js');
 
 module.exports = function (app) {
   app.get('/devices/', function (req, res) {
-    res.render('devices', routesUtilities.getRenderObject(req));
+    switch (service.device_application){
+      case 'devices':
+        return res.render('devices', routesUtilities.getRenderObject(req));
+        break;
+      case 'cdb':
+        return res.redirect(service.cdb.web_portal_url);
+        break;
+      default:
+        return res.send(404, "No valid devices setting has been found");
+    }
   });
 
   app.get('/devices/json', function (req, res) {
