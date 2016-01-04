20901773bf6fe6c469e8e04e6c3a99fd0293bb1a
Dong Liu <edongliu@gmail.com>
2016-01-04 10:05:37 -0500

    add form manager table element

---
+++ public/javascripts/form-manager.js
@@ -0,0 +1,55 @@
+/*global ajax401: false, prefix: false, updateAjaxURL: false, traveler: false*/
+/*global previewColumn: false, referenceFormLinkColumn: false, aliasColumn: false, activatedOnColumn: false, sDomClean: false, titleColumn: false, updatedOnColumn: false, formLinkColumn: false*/
+$(function () {
+
+  ajax401(prefix);
+
+  updateAjaxURL(prefix);
+
+  var activeColumns = [previewColumn, aliasColumn, activatedOnColumn, referenceFormLinkColumn];
+  // fnAddFilterFoot('#active-form', activeColumns);
+  var form = traveler.forms[traveler.activeForm];
+  var active = {
+    activatedOn: form.activatedOn.length ? form.activatedOn : [traveler.createdOn],
+    reference: form.reference || traveler.referenceForm,
+    alias: form.alias || 'not set yet'
+  };
+  var activeTable = $('#active-form').dataTable({
+    aaData: [active],
+    aoColumns: activeColumns,
+    // bAutoWidth: false,
+    sDom: sDomClean
+  });
+
+  var usedColumns = activeColumns;
+  var used = [];
+  traveler.forms.forEach(function (value, index) {
+    if (index !== traveler.activeForm) {
+      used.push(value);
+    }
+  });
+
+  var usedTable = $('#used-forms').dataTable({
+    aaData: used,
+    aoColumns: usedColumns,
+    // bAutoWidth: false,
+    sDom: sDomPage
+  });
+
+  var availableColumns = [previewColumn, titleColumn, updatedOnColumn, formColumn];
+  var availableTable = $('#available-forms').dataTable({
+    sAjaxSource: '/forms/json',
+    sAjaxDataProp: '',
+    // bAutoWidth: false,
+    bProcessing: true,
+    oLanguage: {
+      sLoadingRecords: 'Please wait - loading data from the server ...'
+    },
+    aoColumns: availableColumns,
+    aaSorting: [
+      [2, 'desc']
+    ],
+    sDom: sDomPage
+  });
+
+});
