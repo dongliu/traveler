37f99c666d4f364e3b455f3a84198b27321438a6
Dariusz <dariuszjarosz@me.com>
2016-02-05 13:59:41 -0600

    Add functionality to perform mongodb backups and restorations

--- sbin/configure_paths.sh
+++ sbin/configure_paths.sh
@@ -2,6 +2,8 @@
 
 MY_DIR=`dirname $0` && cd $MY_DIR && MY_DIR=`pwd`
 
+source $MY_DIR/../setup.sh
+
 export TRAVELER_ETC_INIT_DIRECTORY=$TRAVELER_ROOT_DIR/etc/init.d
 
-source $MY_DIR/../setup.sh
+export TRAVELER_BACKUP_DIRECTORY=$TRAVELER_INSTALL_DIR/backup
