f15c2476875e63bef3e9a28653312f33057d7ccb
Dong Liu <edongliu@gmail.com>
2016-03-02 10:29:15 -0500

    add color to package share link

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -430,9 +430,11 @@ var packageConfigLinkColumn = {
 
 var packageShareLinkColumn = {
   sTitle: '',
-  mData: '_id',
-  mRender: function (data, type, full) {
-    return '<a href="' + prefix + '/workingpackages/' + data + '/share/" target="_blank" data-toggle="tooltip" title="share the package"><i class="fa fa-users fa-lg"></i></a>';
+  mData: function (source) {
+    if (source.publicAccess >= 0) {
+      return '<a href="' + prefix + '/workingpackages/' + source._id + '/share/" target="_blank" data-toggle="tooltip" title="share the form" class="text-success"><i class="fa fa-users fa-lg"></i></a>';
+    }
+    return '<a href="' + prefix + '/workingpackages/' + source._id + '/share/" target="_blank" data-toggle="tooltip" title="share the form"><i class="fa fa-users fa-lg"></i></a>';
   },
   bSortable: false
 };
