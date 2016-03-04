35771fa2d74e67336d2b674ad55615761d1c9efe
Dong Liu <edongliu@gmail.com>
2016-03-04 17:43:01 -0500

    add new dom

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -694,6 +694,7 @@ var sDom = "<'row-fluid'<'span6'<'control-group'T>>><'row-fluid'<'span6'l><'span
 var sDom2i = "<'row-fluid'<'span6'<'control-group'T>>><'row-fluid'<'span3'l><'span3'i><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>";
 var sDom2i1p = "<'row-fluid'<'span6'<'control-group'T>>><'row-fluid'<'span3'l><'span3'i><'span3'r><'span3'f>>t<'row-fluid'<'span6'i><'span6'p>>";
 var sDomNoTools = "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>";
+var sDomNoTNoR = "t<'row-fluid'<'span6'i><'span6'p>>";
 var sDomClean = "t";
 var sDomPage = "<'row-fluid'r>t<'row-fluid'<'span6'i><'span6'p>>";
 
