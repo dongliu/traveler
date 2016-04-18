83bbd33c7aa8108c5e4f3985aff618689a836cdb
Dong Liu <edongliu@gmail.com>
2016-04-18 16:30:42 -0400

    update column and button link

--- public/javascripts/working-package-config.js
+++ public/javascripts/working-package-config.js
@@ -1,7 +1,7 @@
 /*eslint max-nested-callbacks: [2, 4]*/
 
 /*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false, FormData: false */
-/*global removeColumn, sequenceColumn, colorColumn, priorityColumn, valueColumn, travelerLinkColumn, aliasColumn, addedByColumn, addedOnColumn, ownerColumn, deviceColumn, sharedWithColumn, sharedGroupColumn, sDomNoTools*/
+/*global removeColumn, sequenceColumn, colorColumn, priorityColumn, valueColumn, travelerLinkColumn, aliasColumn, addedByColumn, addedOnColumn, ownerColumn, deviceTagColumn, sharedWithColumn, sharedGroupColumn, sDomNoTools*/
 /*global moment: false, ajax401: false, updateAjaxURL: false, disableAjaxCache: false, prefix: false*/
 
 function cleanTagForm() {
@@ -148,7 +148,7 @@ $(function () {
     description: $('#description').text()
   };
 
-  var workAoColumns = [removeColumn, sequenceColumn, colorColumn, priorityColumn, valueColumn, travelerLinkColumn, aliasColumn, addedByColumn, addedOnColumn, ownerColumn, deviceColumn, sharedWithColumn, sharedGroupColumn];
+  var workAoColumns = [removeColumn, sequenceColumn, colorColumn, priorityColumn, valueColumn, travelerLinkColumn, aliasColumn, addedByColumn, addedOnColumn, ownerColumn, deviceTagColumn, sharedWithColumn, sharedGroupColumn];
 
   var worksTable = $('#work-table').dataTable({
     sAjaxSource: './works/json',
