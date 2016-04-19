7a8af570489d1ce506bf1d0ee7278caf8843326b
Dong Liu <edongliu@gmail.com>
2016-04-19 15:55:39 -0400

    disable table pagination, add default sort button

--- public/javascripts/working-package.js
+++ public/javascripts/working-package.js
@@ -1,16 +1,13 @@
 /*eslint max-nested-callbacks: [2, 4]*/
 
 /*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false, FormData: false */
-/*global sColumn, pColumn, vColumn, travelerLinkColumn, aliasColumn, workProgressColumn, ownerColumn, deviceTagColumn, manPowerColumn, sDomNoTools*/
-/*global moment: false, ajax401: false, updateAjaxURL: false, disableAjaxCache: false, prefix: false*/
+/*global sColumn, pColumn, vColumn, cColumn, travelerLinkColumn, aliasColumn, workProgressColumn, ownerColumn, deviceTagColumn, manPowerColumn, sDomNoTools*/
+/*global ajax401: false, updateAjaxURL: false, disableAjaxCache: false, prefix: false*/
 
 $(function () {
   updateAjaxURL(prefix);
   ajax401(prefix);
   disableAjaxCache();
-  // $('span.time').each(function () {
-  //   $(this).text(moment($(this).text()).format('dddd, MMMM Do YYYY, h:mm:ss a'));
-  // });
 
   var workAoColumns = [travelerLinkColumn, sColumn, pColumn, vColumn, cColumn, aliasColumn, ownerColumn, deviceTagColumn, manPowerColumn, workProgressColumn];
 
@@ -18,6 +15,7 @@ $(function () {
     sAjaxSource: './works/json',
     sAjaxDataProp: '',
     bAutoWidth: false,
+    bPaginate: false,
     iDisplayLength: 10,
     aLengthMenu: [
       [10, -1],
@@ -33,5 +31,12 @@ $(function () {
       [2, 'asc']
     ],
     sDom: sDomNoTools
+  });
+
+  $('#sort').click(function () {
+    worksTable.fnSort([
+      [1, 'asc'],
+      [2, 'asc']
+    ]);
   });
 });
