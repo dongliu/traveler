504d4dc01ea26b4e5dbae0bdc7e7438add34c00f
Dong Liu <edongliu@gmail.com>
2016-04-19 16:50:08 -0400

    add holder when inited

--- public/javascripts/working-package.js
+++ public/javascripts/working-package.js
@@ -2,7 +2,7 @@
 
 /*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false, FormData: false */
 /*global sColumn, pColumn, vColumn, cColumn, travelerLinkColumn, aliasColumn, workProgressColumn, ownerColumn, deviceTagColumn, manPowerColumn, sDomNoTools*/
-/*global ajax401: false, updateAjaxURL: false, disableAjaxCache: false, prefix: false*/
+/*global ajax401: false, updateAjaxURL: false, disableAjaxCache: false, prefix: false, Holder*/
 
 $(function () {
   updateAjaxURL(prefix);
@@ -26,6 +26,11 @@ $(function () {
     },
     bDeferRender: true,
     aoColumns: workAoColumns,
+    fnInitComplete: function () {
+      Holder.run({
+        images: 'img.user'
+      });
+    },
     aaSorting: [
       [1, 'asc'],
       [2, 'asc']
