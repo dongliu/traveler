147286340d0862a51d8e8184ecf8665f6040cd3a
Dong Liu <edongliu@gmail.com>
2016-05-02 11:06:14 -0400

    eslint

--- model/user.js
+++ model/user.js
@@ -1,6 +1,6 @@
-var mongoose = require('mongoose'),
-  Schema = mongoose.Schema,
-  ObjectId = Schema.Types.ObjectId;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
 
 var user = new Schema({
   _id: String,
