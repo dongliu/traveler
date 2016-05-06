309ebd644c0e37b612f26e03200cdb71cd269273
Dong Liu <edongliu@gmail.com>
2016-05-06 13:58:57 -0400

    update main message

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,14 @@
 /*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false, FormData: false, History: false */
 $(function () {
+  function updateH1m(start, messages) {
+    return function () {
+      $('#h1m').text(messages[start]);
+      start += 1;
+      if (start > messages.length - 1) {
+        start = start % messages.length;
+      }
+    };
+  }
+  var messages = ['Work', 'Design', 'Organize', 'Collaborate'];
+  window.setInterval(updateH1m(0, messages), 10 * 1000);
 });
