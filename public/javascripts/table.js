8d4475055955cb4672b0659511f80e58f1747205
Dong Liu <edongliu@gmail.com>
2016-06-03 17:08:18 -0400

    use term tags

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -591,7 +591,7 @@ var binderProgressColumn = {
 };
 
 var deviceColumn = {
-  sTitle: 'Devices',
+  sTitle: 'Tags',
   mData: function (source, type, val) {
     if (source.devices) {
       return source.devices.join('; ');
