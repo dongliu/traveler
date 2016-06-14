77559339c5e88be2b5ac47f685b37f8d13479baa
Pengquan Wen <wenpq@localhost.localdomain>
2016-06-14 19:18:42 -0400

    bower migrate:bootstap, font-awesome,jquery,bootstap-datepicker

--- app.js
+++ app.js
@@ -93,7 +93,8 @@ app.configure(function () {
     }));
   }
   app.use(express.compress());
-  app.use(express.static(path.join(__dirname, 'public')));
+  // app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'bower_components')));
   app.use(express.favicon(__dirname + '/public/favicon.ico'));
   if (app.get('env') === 'development') {
     app.use(express.logger('dev'));
