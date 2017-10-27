cbfd5f7d0bd44c779443679bb1365002955f1f65
Dariusz Jarosz <dariuszjarosz@me.com>
2017-10-27 15:20:10 -0500

    Add more customizations in configuration. Particualary to allow to disable non-working features in deployment location.

--- config/config.js
+++ config/config.js
@@ -73,16 +73,33 @@ module.exports.load = function () {
 
   // Load view configuration
   var viewConfig = {};
+  viewConfig.showDevice = false;
+  viewConfig.shareUsers = true;
+  viewConfig.shareGroups = true;
+  viewConfig.transferOwnership = true;
+  viewConfig.linkTarget = '_blank';
+  viewConfig.showCCDB = (this.service.device_application === 'devices');
+
   if (this.service.device !== undefined) {
     viewConfig.showDevice = true;
-  } else {
-    viewConfig.showDevice = false;
+  }
+  if (this.ad.shareUsers !== undefined) {
+    viewConfig.shareUsers = this.ad.shareUsers
+  }
+  if (this.ad.shareGroups !== undefined) {
+    viewConfig.shareGroups = this.ad.shareGroups
+  }
+  if (this.ad.transferOwnership !== undefined) {
+      viewConfig.transferOwnership = this.ad.transferOwnership;
   }
   if (this.app.top_bar_urls) {
     viewConfig.topBarUrls = this.app.top_bar_urls;
   }
   if (this.app.deployment_name) {
     viewConfig.deploymentName = this.app.deployment_name;
+  }
+  if (this.app.link_target) {
+    viewConfig.linkTarget = this.app.link_target
   }
   if (this.travelerPackageFile.repository && this.travelerPackageFile.repository.release) {
     viewConfig.appVersion = this.travelerPackageFile.repository.release;
