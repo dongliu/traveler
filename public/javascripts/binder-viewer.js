8575cb278c79c1fc22df99e60fbcc887551e55e6
Dong Liu <edongliu@gmail.com>
2018-09-18 08:12:43 -0700

    comment

--- public/javascripts/binder-viewer.js
+++ public/javascripts/binder-viewer.js
@@ -1,7 +1,7 @@
 /*eslint max-nested-callbacks: [2, 4]*/
 
 /*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false, FormData: false */
-/*global sColumn, pColumn, vColumn, cColumn, travelerLinkColumn, aliasColumn, workProgressColumn, ownerColumn, deviceTagColumn, manPowerColumn, sDomNoTools*/
+/*global sColumn, pColumn, vColumn, cColumn, travelerLinkColumn, aliasColumn, workProgressColumn, ownerColumn, deviceColumn, tagsColumn, manPowerColumn, sDomNoTools*/
 /*global ajax401: false, updateAjaxURL: false, disableAjaxCache: false, prefix: false, Holder*/
 
 $(function () {
