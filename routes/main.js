3657abb335d8696d862a1c5bbafb9c739e2a90a6
Dong Liu <edongliu@gmail.com>
2018-10-07 20:32:50 -0700

    clean up

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,6 @@ var auth = require('../lib/auth');
 
 module.exports = function (app) {
   app.get('/', auth.ensureAuthenticated, function (req, res) {
-    console.log(app.locals.orgName);
     res.render('main', routesUtilities.getRenderObject(req, {
       roles: req.session.roles
     }));
