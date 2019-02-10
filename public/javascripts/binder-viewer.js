5cdb00a2fc787b994d5453b4c3e91a7b0722e18b
Dong Liu <edongliu@gmail.com>
2019-02-10 12:49:59 -0800

    prettier all

--- public/javascripts/binder-viewer.js
+++ public/javascripts/binder-viewer.js
@@ -7,76 +7,87 @@
 function noneSelectedModal() {
   $('#modalLabel').html('Alert');
   $('#modal .modal-body').html('No traveler has been selected!');
-  $('#modal .modal-footer').html('<button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>');
+  $('#modal .modal-footer').html(
+    '<button data-dismiss="modal" aria-hidden="true" class="btn">Return</button>'
+  );
   $('#modal').modal('show');
 }
 
-$(function () {
+$(function() {
   updateAjaxURL(prefix);
   ajax401(prefix);
   disableAjaxCache();
 
-  var workAoColumns = [selectColumn, travelerLinkColumn, sColumn, pColumn, vColumn, cColumn, aliasColumn, ownerColumn, deviceColumn, tagsColumn, keysColumn, manPowerColumn, travelerProgressColumn];
+  var workAoColumns = [
+    selectColumn,
+    travelerLinkColumn,
+    sColumn,
+    pColumn,
+    vColumn,
+    cColumn,
+    aliasColumn,
+    ownerColumn,
+    deviceColumn,
+    tagsColumn,
+    keysColumn,
+    manPowerColumn,
+    travelerProgressColumn,
+  ];
   fnAddFilterFoot('#work-table', workAoColumns);
   var worksTable = $('#work-table').dataTable({
     bAutoWidth: false,
     bPaginate: false,
     iDisplayLength: 10,
-    aLengthMenu: [
-      [10, -1],
-      [10, 'All']
-    ],
+    aLengthMenu: [[10, -1], [10, 'All']],
     oLanguage: {
-      sLoadingRecords: 'Please wait - loading data from the server ...'
+      sLoadingRecords: 'Please wait - loading data from the server ...',
     },
     bDeferRender: true,
     aoColumns: workAoColumns,
-    fnDrawCallback: function () {
+    fnDrawCallback: function() {
       Holder.run({
-        images: 'img.user'
+        images: 'img.user',
       });
     },
-    aaSorting: [
-      [2, 'asc'],
-      [3, 'asc']
-    ],
-    sDom: sDomNoTools
+    aaSorting: [[2, 'asc'], [3, 'asc']],
+    sDom: sDomNoTools,
   });
 
   $.ajax({
     url: './works/json',
     type: 'GET',
-    dataType: 'json'
-  }).done(function (data) {
-    worksTable.fnAddData(data.works);
-    worksTable.fnDraw();
-    $('#value-progress').html(data.valueProgress);
-    $('#traveler-progress').html(data.travelerProgress);
-    $('#input-progress').html(data.inputProgress);
-  }).always();
+    dataType: 'json',
+  })
+    .done(function(data) {
+      worksTable.fnAddData(data.works);
+      worksTable.fnDraw();
+      $('#value-progress').html(data.valueProgress);
+      $('#traveler-progress').html(data.travelerProgress);
+      $('#input-progress').html(data.inputProgress);
+    })
+    .always();
 
-  $('#sort').click(function () {
-    worksTable.fnSort([
-      [1, 'asc'],
-      [2, 'asc']
-    ]);
+  $('#sort').click(function() {
+    worksTable.fnSort([[1, 'asc'], [2, 'asc']]);
   });
 
-  $('span.time').each(function () {
-    $(this).text(moment($(this).text()).format('dddd, MMMM Do YYYY, h:mm:ss a'));
+  $('span.time').each(function() {
+    $(this).text(
+      moment($(this).text()).format('dddd, MMMM Do YYYY, h:mm:ss a')
+    );
   });
 
-  $('button.select-all').click(function () {
+  $('button.select-all').click(function() {
     var activeTable = worksTable;
     fnSelectAll(activeTable, 'row-selected', 'select-row', true);
   });
 
-  $('button.deselect-all').click(function () {
+  $('button.deselect-all').click(function() {
     var activeTable = worksTable;
     fnDeselect(activeTable, 'row-selected', 'select-row');
   });
 
-  $('#report').click(function () {
+  $('#report').click(function() {
     var activeTable = worksTable;
     var selected = fnGetSelected(activeTable, 'row-selected');
     if (selected.length === 0) {
@@ -84,12 +95,14 @@ $(function () {
       return;
     }
     $('#report-form').empty();
-    selected.forEach(function (row) {
+    selected.forEach(function(row) {
       var data = activeTable.fnGetData(row);
-      $('#report-form').append($('<input type="hidden"/>').attr({
-        name: 'travelers[]',
-        value: data._id
-      }));
+      $('#report-form').append(
+        $('<input type="hidden"/>').attr({
+          name: 'travelers[]',
+          value: data._id,
+        })
+      );
     });
     $('#report-form').submit();
   });
--- public/javascripts/form-viewer.js
+++ public/javascripts/form-viewer.js
@@ -2,11 +2,19 @@
 
 function createSideNav() {
   var $legend = $('legend');
-  var $affix = $('<ul class="nav nav-list nav-stacked affix bs-docs-sidenav" data-offset-top="0"></ul>');
+  var $affix = $(
+    '<ul class="nav nav-list nav-stacked affix bs-docs-sidenav" data-offset-top="0"></ul>'
+  );
   var i;
   if ($legend.length > 1) {
     for (i = 0; i < $legend.length; i += 1) {
-      $affix.append('<li><a href="#' + $legend[i].id + '">' + $legend[i].textContent + '</a></li>');
+      $affix.append(
+        '<li><a href="#' +
+          $legend[i].id +
+          '">' +
+          $legend[i].textContent +
+          '</a></li>'
+      );
     }
     $('.sidebar').append($('<div id="affixlist"></div>').append($affix));
     $('body').attr('data-spy', 'scroll');
@@ -14,36 +22,39 @@ function createSideNav() {
   }
 }
 
-$(function () {
-  $('#output').find('img').each(function (index) {
-    var $this = $(this);
-    if ($this.attr('name')) {
-      if ($this.attr('src') === undefined) {
-        $($this.attr('src', prefix + '/formfiles/' + $this.attr('name')));
-        return;
+$(function() {
+  $('#output')
+    .find('img')
+    .each(function(index) {
+      var $this = $(this);
+      if ($this.attr('name')) {
+        if ($this.attr('src') === undefined) {
+          $($this.attr('src', prefix + '/formfiles/' + $this.attr('name')));
+          return;
+        }
+        if ($this.attr('src').indexOf('http') === 0) {
+          $($this.attr('src', prefix + '/formfiles/' + $this.attr('name')));
+          return;
+        }
+        if (prefix && $this.attr('src').indexOf(prefix) !== 0) {
+          $($this.attr('src', prefix + '/formfiles/' + $this.attr('name')));
+          return;
+        }
       }
-      if ($this.attr('src').indexOf('http') === 0) {
-        $($this.attr('src', prefix + '/formfiles/' + $this.attr('name')));
-        return;
-      }
-      if (prefix && $this.attr('src').indexOf(prefix) !== 0) {
-        $($this.attr('src', prefix + '/formfiles/' + $this.attr('name')));
-        return;
-      }
-    }
-  });
+    });
 
   $('input, textarea').removeAttr('disabled');
   createSideNav();
-  $('#show-validation').click(function () {
+  $('#show-validation').click(function() {
     $('.validation').remove();
-    $('#validation').html('<h3>Summary</h3>' + validationMessage(document.getElementById('output')));
+    $('#validation').html(
+      '<h3>Summary</h3>' + validationMessage(document.getElementById('output'))
+    );
     $('#validation').show();
   });
 
-  $('#hide-validation').click(function () {
+  $('#hide-validation').click(function() {
     $('#validation').hide();
     $('.validation').hide();
   });
-
 });
