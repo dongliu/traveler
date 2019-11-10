60d0c61600510e3d33b3a171279aee4c998d23b0
Dong Liu <edongliu@gmail.com>
2019-11-10 10:39:10 -0800

    write to upload in form, refactor

--- routes/form.js
+++ routes/form.js
@@ -298,15 +298,15 @@ module.exports = function(app) {
     '/forms/:id/uploads/',
     auth.ensureAuthenticated,
     reqUtils.exist('id', Form),
-    reqUtils.canReadMw('id'),
+    reqUtils.canWriteMw('id'),
     function(req, res) {
       var doc = req[req.params.id];
       if (_.isEmpty(req.files)) {
-        return res.status(400).send('Expecte One uploaded file');
+        return res.status(400).send('Expect One uploaded file');
       }
 
       if (!req.body.name) {
-        return res.status(400).send('Expecte input name');
+        return res.status(400).send('Expect input name');
       }
 
       var file = new FormFile({
@@ -624,46 +624,44 @@ module.exports = function(app) {
   app.post(
     '/forms/clone/',
     auth.ensureAuthenticated,
-    routesUtilities.filterBody('form'),
+    reqUtils.existSource('form', 'body', Form),
     function(req, res) {
-      Form.findById(req.body.form, function(err, form) {
-        var access = reqUtils.getAccess(req, form);
-        if (access !== -1) {
-          var clonedForm = new Form({
-            title: form.title,
-            createdBy: req.session.userid,
-            createdOn: Date.now(),
-            clonedFrom: form._id,
-            sharedWith: [],
-            sharedGroup: [],
-            html: form.html,
-            tags: form.tags,
+      var access = reqUtils.getAccess(req, form);
+      if (access !== -1) {
+        var clonedForm = new Form({
+          title: form.title,
+          createdBy: req.session.userid,
+          createdOn: Date.now(),
+          clonedFrom: form._id,
+          sharedWith: [],
+          sharedGroup: [],
+          html: form.html,
+          tags: form.tags,
+        });
+
+        clonedForm.save(function(saveErr, createdForm) {
+          if (saveErr) {
+            logger.error(saveErr);
+            return res.status(500).send(err.message);
+          }
+
+          console.log('new form ' + createdForm.id + ' created');
+
+          var url =
+            (req.proxied ? authConfig.proxied_service : authConfig.service) +
+            '/forms/' +
+            createdForm.id +
+            '/';
+          res.set('Location', url);
+          return res.status(201).json({
+            location: url,
           });
-
-          clonedForm.save(function(saveErr, createdForm) {
-            if (saveErr) {
-              logger.error(saveErr);
-              return res.status(500).send(err.message);
-            }
-
-            console.log('new form ' + createdForm.id + ' created');
-
-            var url =
-              (req.proxied ? authConfig.proxied_service : authConfig.service) +
-              '/forms/' +
-              createdForm.id +
-              '/';
-            res.set('Location', url);
-            return res.status(201).json({
-              location: url,
-            });
-          });
-        } else {
-          return res
-            .status(400)
-            .send('you are not authorized to clone this form');
-        }
-      });
+        });
+      } else {
+        return res
+          .status(400)
+          .send('you are not authorized to clone this form');
+      }
     }
   );
 
