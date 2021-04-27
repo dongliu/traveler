56d076e6ca30013306da4240de687035aa80db0e
Dariusz Jarosz <dariuszjarosz@me.com>
2021-04-27 12:13:12 -0500

    Publish an client API for traveler app. Closes #297

---
+++ tools/openapi/setup.py
@@ -0,0 +1,23 @@
+"""
+DEV NOTE: To publish API
+# Update version in this file
+python3 setup.py sdist
+twine upload dist/(specific version file)
+"""
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='Traveler-API',
+      version='1.5.0',
+      packages=find_packages(),
+      py_modules=["traveler_api_factory"],
+      install_requires=['python-dateutil',
+          'urllib3',
+          'six'],
+      license='Copyright (c) 2015 Dong Liu, Dariusz Jarosz',
+      description='Python APIs used to communicate with traveler API.',
+      maintainer='Dariusz Jarosz',
+      maintainer_email='djarosz@aps.anl.gov',
+      url='https://github.com/AdvancedPhotonSource/traveler'
+      )
