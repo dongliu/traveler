1db3b1bb1aefb56be4b7d5723b2289375a37ca16
Dong Liu <edongliu@gmail.com>
2022-04-06 12:45:20 -0700

    avoid all break

--- public/javascripts/pdf.js
+++ public/javascripts/pdf.js
@@ -2,6 +2,7 @@ function createPDF(html, fileName, opts) {
   const opt = {
     ...opts,
     margin: 1,
+    pagebreak: { mode: 'avoid-all' },
     filename: fileName,
     jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
   };
