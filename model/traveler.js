34a77aefd1de5a26b185cfec7eb70aeef7d894c6
Dariusz Jarosz <dariuszjarosz@me.com>
2022-07-20 15:31:44 -0500

    Resolve date validation that end in 0. Closes #327

--- model/traveler.js
+++ model/traveler.js
@@ -13,7 +13,7 @@ var Binder = mongoose.model('Binder');
 
 var TIME_PART_REG_EX = '([0-1][0-9]|2[0-4]):([0-5][0-9])';
 var TIME_REG_EX = '^' + TIME_PART_REG_EX + '$';
-var DATE_REG_EX_START = '^[0-9]{4}-(0[1-9]|1[0-2])-([0-2][1-9]|3[0-1])';
+var DATE_REG_EX_START = '^[0-9]{4}-(0[1-9]|1[0-2])-([0-2][0-9]|3[0-1])';
 var DATE_REG_EX = DATE_REG_EX_START + '$';
 var DATETIME_REG_EX = DATE_REG_EX_START + 'T' + TIME_PART_REG_EX + '$';
 
