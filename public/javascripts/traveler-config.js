7293df491f86e1a350b8ef04cb116586ef9de73a
Dong Liu <edongliu@gmail.com>
2022-11-06 16:38:51 -0800

    Fix form config 104 (#123)
    * edit the text
    
    * display html in form title and desc
    
    * display html in traveler title
    
    * name in title

--- public/javascripts/traveler-config.js
+++ public/javascripts/traveler-config.js
@@ -54,7 +54,7 @@ $(function() {
         data: JSON.stringify(data),
         success: function(json) {
           initValue[that.id] = json[that.id];
-          $(that).text(json[that.id]);
+          $(that).html(json[that.id]);
         },
         error: function(jqXHR) {
           $(that).text(initValue[that.id]);
