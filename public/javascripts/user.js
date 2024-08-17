36b28bef2cdd98f0d87ac6b7f05c3e02e6420911
Dong Liu <edongliu@gmail.com>
2024-08-17 15:39:34 -0700

    eslint fix

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -1,9 +1,9 @@
-/*global updateAjaxURL: false, prefix: false, roles: true, moment: false*/
+/* global updateAjaxURL: false, prefix: false, roles: true, moment: false */
 
 function update(roles) {
-  var i;
+  let i;
   for (i = 0; i < roles.length; i += 1) {
-    $('input[value= "' + roles[i] + '"]').prop('checked', true);
+    $(`input[value= "${roles[i]}"]`).prop('checked', true);
   }
 }
 
@@ -12,7 +12,7 @@ $(function() {
 
   update(roles);
   $('#modify').click(function(e) {
-    var currentRoles = [];
+    const currentRoles = [];
     $('#roles input:checked').each(function() {
       currentRoles.push($(this).val());
     });
@@ -21,7 +21,7 @@ $(function() {
         '<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>Nothing was changed.</div>'
       );
     } else {
-      var request = $.ajax({
+      const request = $.ajax({
         // url: '/users/'+id,
         type: 'PUT',
         async: true,
@@ -34,12 +34,12 @@ $(function() {
       })
         .done(function(json) {
           roles = currentRoles;
-          var timestamp = request.getResponseHeader('Date');
-          var dateObj = moment(timestamp);
+          const timestamp = request.getResponseHeader('Date');
+          const dateObj = moment(timestamp);
           $('#message').append(
-            '<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>The modification was saved at ' +
-              dateObj.format('HH:mm:ss') +
-              '.</div>'
+            `<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>The modification was saved at ${dateObj.format(
+              'HH:mm:ss'
+            )}.</div>`
           );
         })
         .fail(function(jqXHR, status, error) {
