cf7039da11463898158a0d66661ce78f082b5f56
Sam Clark <sclark@bnl.gov>
2024-08-20 11:26:54 -0500

    Began adding mail infrastructure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,8 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs", globals: globals.node } },
+  { files: ["**/public/**/*.js"], languageOptions: { globals: { ...globals.browser, ...globals.jquery } } },
   pluginJs.configs.recommended,
+  { rules: { "no-unused-vars": "off" } },
 ];
