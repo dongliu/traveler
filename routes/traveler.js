2e17ee08fdbdd42c7bdaeee9c746d39f0c5149b5
Sam Clark <sclark@bnl.gov>
2024-10-21 16:39:33 -0400

    Added notification to workers when traveler is rejected

--- routes/traveler.js
+++ routes/traveler.js
@@ -1063,6 +1063,21 @@ module.exports = function(app) {
               <a href="${travelerLink}">${travelerLink}</a>`,
             });
           });
+        } else if (doc.status == 1 && oldStatus == 1.5) {
+          const workerIds = doc.manPower.map(user => user._id);
+          User.find({ _id: { $in: workerIds } }).then(users => {
+            const travelerLink = `${req.protocol}://${req.get(
+              'host'
+            )}/travelers/${doc._id}/`;
+            const emails = users.map(user => user.email);
+            sendNotification({
+              recipients: emails,
+              subject: 'Traveler Rejected',
+              html: `The traveler "${doc.title}" was sent back for more work. <br/>
+              Please visit the traveler at this link: <br/>
+              <a href="${travelerLink}">${travelerLink}</a>`,
+            });
+          });
         }
         return res.status(200).send(`status updated to ${req.body.status}`);
       });
