9ef390cef89e6c64226da1757113bf46818efcd4
Dong Liu <edongliu@gmail.com>
2025-12-20 19:26:19 -0800

    add mocha globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs", globals: globals.node } },
-  { files: ["**/public/**/*.js"], languageOptions: { globals: { ...globals.browser, ...globals.jquery } } },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node, ...globals.mocha },
+    },
+  },
+  {
+    files: ['**/public/**/*.js'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.jquery },
+    },
+  },
   pluginJs.configs.recommended,
-  { rules: { "no-unused-vars": "off" } },
+  { rules: { 'no-unused-vars': 'off' } },
 ];
