4110dee72444d6de4e759f091b12cd854a6dd851
Dong Liu <edongliu@gmail.com>
2025-12-24 20:28:42 -0800

    default roles not in modify list

--- routes/admin.js
+++ routes/admin.js
@@ -3,24 +3,26 @@ const jade = require('jade');
 var auth = require('../lib/auth');
 var routesUtilities = require('../utilities/routes.js');
 const config = require('../config/config.js');
-
+const reqUtils = require('../lib/req-utils');
+const { Roles } = require('../lib/role.js');
 const rolesHtml = jade.compileFile(`${__dirname}/../views/roles.jade`);
 
 module.exports = function(app) {
-  app.get('/admin/', auth.ensureAuthenticated, function(req, res) {
-    if (
-      res.locals.roles === undefined ||
-      res.locals.roles.indexOf('admin') === -1
-    ) {
-      return res.status(403).send('only admin allowed');
+  app.get(
+    '/admin/',
+    auth.ensureAuthenticated,
+    reqUtils.requireAdmin(),
+    function(req, res) {
+      const supportedRoles = Object.keys(config.permission).filter(role =>
+        Roles.includes(role)
+      );
+      return res.render(
+        'admin',
+        routesUtilities.getRenderObject(req, {
+          supportedRoles,
+          rolesHtml: rolesHtml({ supportedRoles }),
+        })
+      );
     }
-    const supportedRoles = Object.keys(config.permission);
-    return res.render(
-      'admin',
-      routesUtilities.getRenderObject(req, {
-        supportedRoles,
-        rolesHtml: rolesHtml({ supportedRoles }),
-      })
-    );
-  });
+  );
 };
