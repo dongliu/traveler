e8f08fad024b68d601ac47ae74876831914aff7a
Dong Liu <edongliu@gmail.com>
2026-01-29 19:57:37 -0800

    get supported roles and test

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,8 @@ const jade = require('jade');
 
 var auth = require('../lib/auth');
 var routesUtilities = require('../utilities/routes.js');
-const config = require('../config/config.js');
 const reqUtils = require('../lib/req-utils');
-const { Roles } = require('../lib/role.js');
+const { getSupportedRoles } = require('../lib/role.js');
 const rolesHtml = jade.compileFile(`${__dirname}/../views/roles.jade`);
 
 module.exports = function(app) {
@@ -13,9 +12,7 @@ module.exports = function(app) {
     auth.ensureAuthenticated,
     reqUtils.requireAdmin(),
     function(req, res) {
-      const supportedRoles = Object.keys(config.permission).filter(role =>
-        Roles.includes(role)
-      );
+      const supportedRoles = getSupportedRoles();
       return res.render(
         'admin',
         routesUtilities.getRenderObject(req, {
