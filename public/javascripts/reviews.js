3e8edf748417e82595f4a93dc0a72206d6f188bb
Dong Liu <edongliu@gmail.com>
2026-01-31 11:20:11 -0800

    add traveler review page

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -44,6 +44,48 @@ $(function() {
   const tables = [];
   Table.initTableIfExists($('#form-table'), reviewTableConfig, tables);
 
+  const reviewTravelersAoColumns = [
+    travelerLinkColumn,
+    titleColumn,
+    docNoColumn,
+    travelerVersionColumn,
+    deviceColumn,
+    tagsColumn,
+    reviewRequestedByColumn,
+    reviewRequestedOnColumn,
+  ];
+  const reviewTravelersTableConfig = {
+    sAjaxSource: '/reviews/travelers/active/json',
+    sAjaxDataProp: '',
+    fnDrawCallback() {
+      Holder.run({
+        images: 'img.user',
+      });
+    },
+    bAutoWidth: false,
+    iDisplayLength: 10,
+    aLengthMenu: [
+      [10, 50, 100, -1],
+      [10, 50, 100, 'All'],
+    ],
+    oLanguage: {
+      sLoadingRecords: 'Please wait - loading data from the server ...',
+    },
+    bDeferRender: true,
+    aoColumns: reviewTravelersAoColumns,
+    sDom: sDomNoTools,
+  };
+  Table.sortByColumn(
+    reviewTravelersTableConfig,
+    reviewRequestedOnColumn,
+    'desc'
+  );
+  Table.initTableIfExists(
+    $('#traveler-table'),
+    reviewTravelersTableConfig,
+    tables
+  );
+
   // binding events
   filterEvent();
 });
