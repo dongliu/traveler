9abdc3f4e4faf07f5832bf96cd431425467068b4
Dong Liu <edongliu@gmail.com>
2026-02-01 20:49:42 -0800

    reuse review for traveler

--- routes/traveler.js
+++ routes/traveler.js
@@ -25,6 +25,8 @@ const Traveler = mongoose.model('Traveler');
 const TravelerData = mongoose.model('TravelerData');
 const TravelerNote = mongoose.model('TravelerNote');
 const Log = mongoose.model('Log');
+const reviewLib = require('../lib/review');
+const travelerLib = require('../lib/traveler');
 
 const { TravelerError } = require('../lib/error');
 const { stateTransition } = require('../model/traveler');
@@ -151,54 +153,6 @@ function redirectPreview(req, res, next) {
     );
   }
   return next();
-}
-
-/**
- * When a traveler is submitted for approval, find the form from the form table
- * with activeForm as id, populate the createdBy user with name and email,
- * and send notification email to the user.
- * @param {Object} traveler - the traveler object
- * @param {Object} req - the request object (for building the link)
- * @return {Promise<boolean>} true if email is sent, otherwise false
- */
-async function submitForApproval(traveler, req) {
-  try {
-    // Find the form from the form table with activeForm as id
-    const form = await Form.findById(traveler.activeForm).populate(
-      'createdBy',
-      'name email'
-    );
-    if (!form) {
-      return false;
-    }
-
-    // Get the user who created the form
-    const user = form.createdBy;
-    if (!user || !user.email) {
-      return false;
-    }
-
-    // add a review request
-    await traveler.requestReview(req.session.userid, user);
-
-    // Build the traveler link
-    const travelerLink = `${req.protocol}://${req.get('host')}/travelers/${
-      traveler._id
-    }/`;
-
-    // Send notification email
-    return await sendNotification({
-      recipients: user.email,
-      subject: 'Traveler Submitted for Approval',
-      text: `The traveler "${traveler.title}" has been submitted for approval.\nPlease review the traveler at this link: \n${travelerLink}`,
-      html: `The traveler "${traveler.title}" has been submitted for approval. <br/>
-      Please review the traveler at this link: <br/>
-      <a href="${travelerLink}">${travelerLink}</a>`,
-    });
-  } catch (error) {
-    logger.error(error);
-    return false;
-  }
 }
 
 module.exports = function(app) {
@@ -521,7 +475,9 @@ module.exports = function(app) {
             canStart: doc.sharedWith.some(
               doc => doc._id === req.session.userid
             ),
+            isReviewer: doc.isReviewer(req.session.userid),
             traveler: doc,
+            review: doc.__review,
             formHTML:
               doc.forms.length === 1
                 ? doc.forms[0].html
@@ -587,6 +543,27 @@ module.exports = function(app) {
           'data'
         )
       );
+    }
+  );
+
+  // add a new review request
+  app.post(
+    '/travelers/:id/review/results',
+    auth.ensureAuthenticated,
+    reqUtils.exist('id', Traveler),
+    // only available when under review
+    reqUtils.status('id', [1.5]),
+    function(req, res, next) {
+      const traveler = req[req.params.id];
+      const isReviewer = traveler.isReviewer(req.session.userid);
+      if (!isReviewer) {
+        return res.status(401).send('only reviewer can submit');
+      }
+      return next();
+    },
+    async function(req, res) {
+      const traveler = req[req.params.id];
+      await reviewLib.addReviewResult(req, res, traveler);
     }
   );
 
@@ -1049,78 +1026,8 @@ module.exports = function(app) {
     reqUtils.exist('id', Traveler),
     reqUtils.canWriteMw('id'),
     reqUtils.archived('id', false),
-    function(req, res) {
-      const doc = req[req.params.id];
-
-      if ([1, 1.5, 2, 3, 4].indexOf(req.body.status) === -1) {
-        return res.status(400).send('invalid status');
-      }
-
-      if (doc.status === req.body.status) {
-        return res.status(204).send();
-      }
-
-      const target = _.find(stateTransition, function(t) {
-        return t.from === doc.status;
-      });
-
-      debug(target);
-      if (target.to.indexOf(req.body.status) === -1) {
-        return res.status(400).send('invalid status change');
-      }
-
-      // authorize approve or reject traveler
-      if (
-        doc.status === 1.5 &&
-        (req.body.status === 2 || req.body.status === 1) &&
-        !routesUtilities.hasPermission(req, Approve_travelers)
-      ) {
-        return res
-          .status(403)
-          .send('You are not authorized to change the status. ');
-      }
-
-      // request more work or archive a completed traveler
-      if (
-        doc.status === 2 &&
-        (req.body.status === 4 || req.body.status === 1) &&
-        !routesUtilities.hasPermission(req, Approve_travelers)
-      ) {
-        return res
-          .status(403)
-          .send('You are not authorized to change the status. ');
-      }
-
-      const oldStatus = doc.status;
-      doc.status = req.body.status;
-      doc.updatedBy = req.session.userid;
-      doc.updatedOn = Date.now();
-      mqttUtilities.postTravelerStatusChangedMessage(doc);
-      return doc.save(function(saveErr) {
-        if (saveErr) {
-          logger.error(saveErr);
-          return res.status(500).send(saveErr.message);
-        }
-        if (doc.status === 1.5) {
-          submitForApproval(doc, req);
-        } else if (doc.status == 1 && oldStatus == 1.5) {
-          const workerIds = doc.manPower.map(user => user._id);
-          User.find({ _id: { $in: workerIds } }).then(users => {
-            const travelerLink = `${req.protocol}://${req.get(
-              'host'
-            )}/travelers/${doc._id}/`;
-            const emails = users.map(user => user.email);
-            sendNotification({
-              recipients: emails,
-              subject: 'Traveler Rejected',
-              html: `The traveler "${doc.title}" was sent back for more work. <br/>
-              Please visit the traveler at this link: <br/>
-              <a href="${travelerLink}">${travelerLink}</a>`,
-            });
-          });
-        }
-        return res.status(200).send(`status updated to ${req.body.status}`);
-      });
+    function updateStatus(req, res) {
+      return travelerLib.updateStatus(req, res);
     }
   );
 
