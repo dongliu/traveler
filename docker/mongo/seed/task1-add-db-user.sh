9b5b97bbfa8e9e6010b1444cd7f3813fbc910f9a
Dong Liu <edongliu@gmail.com>
2026-04-16 23:07:53 -0700

    specify docker from .env

--- docker/mongo/seed/task1-add-db-user.sh
+++
@@ -1,3 +0,0 @@
-mongoshell="use admin"
-mongoshell="$mongoshell \n db.createUser( { user: \"traveler\", pwd: \"travelerpassword\", roles: [ { role: \"readWrite\", db: \"traveler\" } ] } )"
-echo -e $mongoshell | mongo -u root -p rootspass --authenticationDatabase admin
--- docker/mongo/seed/task2-add-traveler-admin.sh
+++
@@ -1,3 +0,0 @@
-# TODO: add more seeding data for admin users
-mongoshell="use traveler"
-echo -e $mongoshell | mongo -u traveler -p travelerpassword --authenticationDatabase admin
